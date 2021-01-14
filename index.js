6344c8a4148f180f99e52c96879ced7611034efa
Marie-Ève Noël <67923392+nonowelle@users.noreply.github.com>
2021-01-14 13:14:14 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+//HOVER EFFECT NAV
+$("a").hover(function () {
+    $(this).css("color", "#E5E5E5")
+},
+    function () {
+        $(this).css("color", "#FCAF58")
+    }
+);
+
+$('.fleche').hover(function () {
+    $(this).css("color", "#E5E5E5")
+},
+    function () {
+        $(this).css("color", "#FCAF58")
+    }
+);
+
+$('.rond-fleche').hover(function () {
+    $(this).css("border-color", "#E5E5E5")
+
+},
+    function () {
+        $(this).css("border-color", "#FCAF58")
+    }
+);
+
